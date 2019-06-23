c07ee98148933ef6959525ebc745f2fec3921ed8
petergit999 <52128830+petergit999@users.noreply.github.com>
2019-06-23 21:32:50 +0800

    Add files via upload

---
+++ TEST03/src/main/java/com/crunchify/restjersey/CtoFService.java
@@ -0,0 +1,43 @@
+package com.crunchify.restjersey;
+
+import java.util.regex.Pattern;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+ 
+@Path("/ctofservice")
+public class CtoFService {
+	@GET
+	@Produces("application/xml")
+	public String convertCtoF() {
+ 
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+ 
+	@Path("{c}")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoFfromInput(@PathParam("c") Double c) {
+		
+		Double fahrenheit = (double) 0;
+		Double celsius = c;
+		try {
+			
+			fahrenheit = ((celsius * 9) / 5) + 32;
+	 
+		} catch (Exception e) {
+			String result = "500";
+			return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+		}
+		
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+}
